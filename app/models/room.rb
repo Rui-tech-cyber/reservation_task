653542a2fb13103cf67308b39028599b898cc924
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-08-09 03:39:06 +0900

    各モデルのバリデーション追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
   belongs_to :user
   has_many :reservations
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, length: { maximum: 1000 }, allow_blank: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :address, presence: true, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :rooms
   has_many :reservations
 
-  #deviseの設定も含める
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :profile, length: { maximum: 300 }, allow_blank: true
 end
