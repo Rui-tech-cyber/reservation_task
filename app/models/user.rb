447aa2a20831d1596bb32bd30716b94c57b049bb
ナカムラルイ <nakamura@nakamuraruinoMacBook-Pro.local>
2025-08-10 06:41:53 +0900

    deviseの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   has_many :reservations
 
   validates :name, presence: true, length: { maximum: 30 }
+  validates :phone_number, presence: true
   validates :profile, length: { maximum: 300 }, allow_blank: true
 end
